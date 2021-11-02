4092181e9d88842009d5cc3f88582a596e425c22
ray <86501179+TheIIISociety@users.noreply.github.com>
2021-11-02 02:38:17 +0100

    code...

---
+++ www/js/signup.js
@@ -0,0 +1,13 @@
+
+const signInBtn = document.querySelector("#sign-in-btn");
+const signUpBtn = document.querySelector("#sign-up-btn");
+const container = document.querySelector(".container");
+
+signUpBtn.addEventListener("click", () => {
+    container.classList.add("sign-up-mode");
+});
+
+signInBtn.addEventListener("click", () => {
+    container.classList.remove("sign-up-mode");
+});
+
