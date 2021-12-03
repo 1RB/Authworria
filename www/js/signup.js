a1245627a7d0d20a344c393cc9f8efee60f0a0a1
InimicalPart <inimicalpart@gmail.com>
2021-12-03 02:05:30 +0100

    adding profle ish

--- www/js/signup.js
+++ www/js/signup.js
@@ -1,13 +1,11 @@
-
 const signInBtn = document.querySelector("#sign-in-btn");
 const signUpBtn = document.querySelector("#sign-up-btn");
 const container = document.querySelector(".container");
 
 signUpBtn.addEventListener("click", () => {
-    container.classList.add("sign-up-mode");
+  container.classList.add("sign-up-mode");
 });
 
 signInBtn.addEventListener("click", () => {
-    container.classList.remove("sign-up-mode");
+  container.classList.remove("sign-up-mode");
 });
-
